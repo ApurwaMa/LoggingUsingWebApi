b43067f1321e11853e8d8d451db511bc127046af
Apurva Gupta <agupta1@lseg.stockex.local>
2018-07-16 05:44:00 -0700

    added project so that client can consume logging mehods by web api

---
+++ LoggingUsingWebApi/ClientLoggingUsingWebApi/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ClientLoggingUsingWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                throw new Exception("Divide By Zero Exception", new DivideByZeroException());
+            }
+            catch (Exception ex)
+            {
+                LoggingGetInfoAfterLoggingToFile(ex.Message).Wait();
+
+                LoggingGetInfoAfterLoggingToEventViewer(ex.Message).Wait();
+
+                LoggingGetInfoUsingFileStream(ex.Message).Wait();
+                Console.WriteLine("Press any key to close the application");
+                Console.ReadKey();
+            }
+        }
+
+        static async Task LoggingGetInfoAfterLoggingToFile(string exception)
+        {
+            HttpClient cons = new HttpClient();
+            cons.BaseAddress = new Uri("http://localhost:62292/");
+            cons.DefaultRequestHeaders.Accept.Clear();
+            cons.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            using (cons)
+            {
+                HttpResponseMessage res = await cons.GetAsync("api/Logging/?exception=" + exception);
+                res.EnsureSuccessStatusCode();
+                if (res.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("successfully logged");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static async Task LoggingGetInfoAfterLoggingToEventViewer(string exception)
+        {
+            HttpClient cons = new HttpClient();
+            cons.BaseAddress = new Uri("http://localhost:62292/");
+            cons.DefaultRequestHeaders.Accept.Clear();
+            cons.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            using (cons)
+            {
+                HttpResponseMessage res = await cons.GetAsync("api/Logging/?message=" + exception + "&source=EventSource");
+                res.EnsureSuccessStatusCode();
+                if (res.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("successfully logged");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static async Task LoggingGetInfoUsingFileStream(string exception)
+        {
+            HttpClient cons = new HttpClient();
+            cons.BaseAddress = new Uri("http://localhost:62292/");
+            cons.DefaultRequestHeaders.Accept.Clear();
+            cons.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            using (cons)
+            {
+                HttpResponseMessage res = await cons.GetAsync("api/Logging/?strFileName=Error&strMessage=" + exception);
+                res.EnsureSuccessStatusCode();
+                if (res.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("successfully logged");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ LoggingUsingWebApi/LoggingUsingWebApi/Controllers/LoggingController.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LoggingUsingWebApi.Controllers
+{
+    public class LoggingController : ApiController
+    {
+
+        [ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> GetInfoAfterLoggingToFile(string exception)
+        {
+            ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+            await Task.Run(() => logger.Error(exception));
+            return Ok("Logged Error To File Using Log4Net");
+        }
+
+
+        [ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> GetInfoLoggingFuntionToEventViewer(string message,string source)
+        {
+            EventLog m_EventLog = new EventLog("");
+            m_EventLog.Source = source;
+            await Task.Run(() => m_EventLog.WriteEntry("Reading text file failed " + message,
+                EventLogEntryType.FailureAudit));
+            return Ok("Logged Error To EventViewer");
+        }
+
+
+        [ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> GetInfoUsingFileStream(string strFileName, string strMessage)
+        {
+            try
+            {
+                FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", Path.GetTempPath(), strFileName), FileMode.Append, FileAccess.Write);
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(strMessage);
+                objStreamWriter.Close();
+                await Task.Run(() => objFilestream.Close());
+                return Ok("Logged Error Using FileStream");
+            }
+            catch (Exception ex)
+            {
+                return Ok("Exception While Logging Error Using FileStream");
+            }
+        }
+
+    }
+}
